566faff631ba329b164a737bfb2aa3b92c28712f
Lukas <xlukas9@gmail.com>
2019-10-25 21:45:54 +0200

    can coder fised

--- Tests/test/test_coder.c
+++ Tests/test/test_coder.c
@@ -3,7 +3,7 @@
 #include "adc.h"
 
 
-coding_parameter test_uint16_5_5 ={.bits =16, .offset = 0,.range = 5.5};
+coding_parameter test_uint16_128 ={.bits =16, .offset = 0,.range = 128};
 coding_parameter test_int16_5_5 = {.bits = 16, .offset = 127,.range =5.5};
 void setUp(void)
 {
@@ -14,21 +14,21 @@ void tearDown(void)
 }
 void test_coder_proper_values(void){
 	//arrange
-	float test_float = 1.51;
+	float test_float = 78.0;
 
 	//act
-	uint16_t test_result = encode_float_to_uint16(test_float,test_uint16_5_5);
+	uint16_t test_result = encode_float_to_uint16(test_float,test_uint16_128);
 	//assert
 
-	TEST_ASSERT_EQUAL_UINT16(17992,test_result);
+	TEST_ASSERT_EQUAL_UINT16(39936,test_result);
 }
 
 void test_coder_inproper_values(void){
 	//arrange
-	float test_float = 6.2;
+	float test_float = 150.0;
 
 	//act
-	uint16_t test_result = encode_float_to_uint16(test_float,test_uint16_5_5);
+	uint16_t test_result = encode_float_to_uint16(test_float,test_uint16_128);
 	//assert
 
 	TEST_ASSERT_EQUAL_UINT16(0,test_result);
@@ -40,10 +40,12 @@ void test_encode_frame_big_endian_proper_values(void){
 	uint8_t test_data_length = 3;
 
 	//act
-	uint64_t test_results = encode_frame_big_endian(test_data,test_data_length);
-
+	uint8_t* test_results = encode_frame_big_endian(test_data,test_data_length);
 	//assert
-	TEST_ASSERT_EQUAL_UINT32(0xB0A1FF, test_results);
+	TEST_ASSERT_EQUAL_HEX(0xB0, test_results[0]);
+	TEST_ASSERT_EQUAL_HEX(0xA1, test_results[1]);
+	TEST_ASSERT_EQUAL_HEX(0xFF, test_results[2]);
+	free(test_results);
 }
 
 void test_encode_frame_big_endian_zero_length_value(void){
@@ -55,6 +57,6 @@ void test_encode_frame_big_endian_zero_length_value(void){
 	uint64_t test_results = encode_frame_big_endian(test_data,test_data_length);
 
 	//assert
-	TEST_ASSERT_EQUAL_UINT32(0, test_results);
+	TEST_ASSERT_NULL(test_results);
 }
 
