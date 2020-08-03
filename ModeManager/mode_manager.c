1939594f6667d77d7bf496b338baf8587fab78a8
igorpieniek <pieniek97@gmail.com>
2020-08-03 12:06:22 +0200

    add all mode/can manager functions
    - without initializing tim4 (idle timer)

--- ModeManager/mode_manager.c
+++ ModeManager/mode_manager.c
@@ -16,9 +16,99 @@ void modeManagerInit(){
 
 
 void getData_Rx(uint32_t frame_id, uint8_t* data, uint8_t dlc){
-
+	if (frame_id == STATUS_FRAME_ID) convertStatusData_Rx( data); // function also update status in modemanager
+	else if ( frame_id == VELOCITY_FRAME_ID ) 		setVelocity( data , RC );
+	else if ( frame_id == I3_VELOCITY_FRAME_ID )	setVelocity( data ,I3  );
+	else if ( frame_id == TURN_FRAME_ID )			setTurn( data, RC  );
+	else if ( frame_id == I3_TURN_FRAME_ID )	 	setTurn( data, I3  );
 }
 
+void convertStatusData_Rx(uint8_t * data){
+
+	uint8_t status = data[STATUS_MODE_BYTE];
+	uint8_t permition = data[STATUS_PERMITION_BYTE];
+	statusUpdate(getRCmodeStatus_Rx( status ), getDriveModestatus_Rx( permition ) );
+}
+
+void setVelocity(uint8_t* vel, enum MSG_ORIGIN origin){
+	if (velocityPermission(origin)) sendMsg(VELOCITY, vel);
+}
+void setTurn(uint8_t* turn, enum MSG_ORIGIN origin){
+	if (turnPermission(origin)) sendMsg(TURN,turn);
+}
+
+void sendMsg(enum SEND_MODE mode, uint8_t * msgData){
+	if (mode == TURN && sizeof(msgData)==STEERING_FRAME_LENGTH ){
+		hal_can_send( STEERING_VELOCITY_FRAME_ID, STEERING_FRAME_LENGTH , msgData);
+	}
+	else if (mode == VELOCITY && sizeof(msgData)==STEERING_FRAME_LENGTH){
+		hal_can_send( STEERING_TURN_FRAME_ID,  STEERING_FRAME_LENGTH , msgData);
+	}
+}
+
+
+enum RC_MODE getRCmodeStatus_Rx(uint8_t status){
+	if (status == JOYSTICK_MODE_MSG ) 		return MODE_JOYSICK;
+	else if(status == ACRO_MODE_MSG ) 		return MODE_ACRO;
+	else if(status == SEMI_MODE_MSG ) 		return MODE_SEMI;
+	else if(status == AUTONOMOUS_MODE_MSG ) return MODE_AUTONOMOUS;
+}
+
+enum DRIVE_MODE getDriveModestatus_Rx(uint8_t permition){
+	if (permition == ENABLE_DRIVE_MSG  ) 		return EN;
+	else if(permition ==  DISABLE_DRIVE_MSG ) 	return DIS;
+}
+
+
+void statusUpdate(enum RC_MODE RCstatus, enum DRIVE_MODE drivestatus){
+	RCmode = RCstatus;
+	driveMode = drivestatus;
+	if (RCstatus !=MODE_JOYSICK){
+		startIdleTimer();
+	}
+	else stopIdleTimer();
+}
+void startIdleTimer(){
+	HAL_TIM_Base_Start_IT(&IDLE_TIMER);
+}
+
+void stopIdleTimer(){
+	HAL_TIM_Base_Stop_IT(&IDLE_TIMER);
+	resetIdleTimer();
+}
+void resetIdleTimer(){
+	__HAL_TIM_SET_COUNTER(&IDLE_TIMER, 0);
+}
+
+uint8_t velocityPermission(enum MSG_ORIGIN origin){
+	resetIdleTimer();
+	if (driveMode == EN){
+		if(origin == RC && (RCmode == MODE_ACRO ||
+  	   	   	     	 	 	RCmode == MODE_SEMI) ){
+			return 1;
+		}
+		else if(origin == I3 && (RCmode == MODE_AUTONOMOUS) ){
+			return 1;
+		}
+		else return 0;
+	}
+	else return 0;
+}
+
+uint8_t turnPermission(enum MSG_ORIGIN origin){
+	resetIdleTimer();
+	if (driveMode == EN){
+		if(origin == RC && RCmode == MODE_ACRO){
+			return 1;
+		}
+		else if(origin == I3 && (RCmode == MODE_AUTONOMOUS ||
+				   	   	   	     RCmode == MODE_SEMI) ){
+			return 1;
+		}
+		else return 0;
+	}
+	else return 0;
+}
 
 uint8_t isJoystickMode(){
 	if (driveMode == EN && RCmode == MODE_JOYSICK){
--- ModeManager/mode_manager.h
+++ ModeManager/mode_manager.h
@@ -10,14 +10,34 @@
 
 #include "main.h"
 #include "tim.h"
+#include "can.h"
 #include "stm32f1xx_hal_tim.h"
 
 #define IDLE_TIMER htim4
 #define PROCESS_TIMER htim3
 
-void modeManagerInit();
-uint8_t isJoystickMode();
-void getData_Rx(uint32_t frame_id, uint8_t* data, uint8_t dlc);
+#define VELOCITY_FRAME_ID   0x23E //TODO
+#define TURN_FRAME_ID  		0x24D
+#define STATUS_FRAME_ID  	0x25D
+
+#define I3_VELOCITY_FRAME_ID    0x26D
+#define I3_TURN_FRAME_ID  		0x27D
+
+#define STATUS_MODE_BYTE      1
+#define STATUS_PERMITION_BYTE 3
+
+#define JOYSTICK_MODE_MSG 	0x00
+#define ACRO_MODE_MSG		0x01
+#define SEMI_MODE_MSG		0x02
+#define AUTONOMOUS_MODE_MSG 0x03
+
+#define ENABLE_DRIVE_MSG  	0x01
+#define DISABLE_DRIVE_MSG 	0x00
+
+// Tx
+#define STEERING_FRAME_LENGTH 4
+#define STEERING_VELOCITY_FRAME_ID  0x21D //old x axis
+#define STEERING_TURN_FRAME_ID   	0x22D // old y axis
 
 enum RC_MODE{
 	MODE_JOYSICK = 0,
@@ -35,6 +55,32 @@ enum MSG_ORIGIN{
 	RC = 0,
 	I3
 };
+
+enum SEND_MODE{
+	TURN = 0,
+	VELOCITY,
+	STATUS
+};
+
+void modeManagerInit();
+uint8_t isJoystickMode();
+void getData_Rx(uint32_t frame_id, uint8_t* data, uint8_t dlc);
+void convertStatusData_Rx(uint8_t * data);
+enum RC_MODE getRCmodeStatus_Rx(uint8_t status);
+enum DRIVE_MODE getDriveModestatus_Rx(uint8_t permition);
+void statusUpdate(enum RC_MODE RCstatus, enum DRIVE_MODE drivestatus);
+
+void setVelocity(uint8_t* vel, enum MSG_ORIGIN origin);
+void setTurn(uint8_t* turn, enum MSG_ORIGIN origin);
+
+uint8_t turnPermission(enum MSG_ORIGIN origin);
+uint8_t velocityPermission(enum MSG_ORIGIN origin);
+
+void sendMsg(enum SEND_MODE mode, uint8_t * msgData);
+
+void startIdleTimer();
+void stopIdleTimer();
+void resetIdleTimer();
 
 
 
