5f5b41202d3df0f27ceb455a0d23c682ae0c4769
igorpieniek <pieniek97@gmail.com>
2020-12-05 10:14:14 +0100

    Verified #2 change period of dangerous state to 0.5s

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -36,7 +36,7 @@ void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 8000-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1500-1;
+  htim2.Init.Period = 500-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
